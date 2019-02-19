08b1b80882370250d6daf917097f3140cef6c498
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-02-19 13:03:25 -0500

    Update game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+alert ("Press any key to get started.");
+
+var wins = ( )  // the number of times the user guesses correctly
+var loses = ()  // the number of times the user guesses incorrectly
+
+var gameWords = ["Rodney", "Tealc", "stargate", "Colnel ONeil","Daniel Jackson", "Colnel Carter", "Colonel Shepard", " The Wraith", "Asgard", "Ronon Dex", "Zero Point Module", "The Ancients", "Replicators", "Ascend", "Chevron","Teyla", "SG1", "Atlantis"];
+
+var keyPress = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var userKeyPress;
+var isTired = True;
