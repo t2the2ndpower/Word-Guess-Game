45f76460316c32f623a4dfad1b39a73557298477
Technoladie <38797497+t2the2ndpower@users.noreply.github.com>
2019-02-20 17:43:46 -0500

    Game Updates and Images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,74 @@
+// My cool crafty guess game.
+
+//Ask users if they want to play the game
+
+confirm ("Do you want to play, 'Guess the Technique'?");
+
+
+
 alert ("Press any key to get started.");
 
-var wins = ( )  // the number of times the user guesses correctly
-var loses = ()  // the number of times the user guesses incorrectly
+var wins = 0;  // the number of times the user guesses correctly
+var loses = 0;  // the number of times the user guesses incorrectly
 
-var gameWords = ["Rodney", "Tealc", "stargate", "Colnel ONeil","Daniel Jackson", "Colnel Carter", "Colonel Shepard", " The Wraith", "Asgard", "Ronon Dex", "Zero Point Module", "The Ancients", "Replicators", "Ascend", "Chevron","Teyla", "SG1", "Atlantis"];
+var gameWords = [
+    {
+        pctname: 'Mica Shift',
+        pctpic: '#',
+        pctdesc:'This is a description of MicaShift'
+    },
+    {
+        pctname: 'Skinner Blend',
+        pctpic: '#',
+        pctdesc:'This is a description for casey'
+    },
+    {
+        pctname: 'Cane',
+        pctpic: '#',
+        pctdesc:'This is a description for casey'
+    },
+    {
+        pctname: 'Mokume Gane',
+        pctpic: '#',
+        pctdesc:'This is a description for casey'
+    },
+    {
+        pctname: 'Sutton Slice',
+        pctpic: '#',
+        pctdesc:'This is a description for casey'
+    },
+    {
+        pctname: 'Image Transfer',
+        pctpic: '#',
+        pctdesc:'This is a description for casey'
+    },
+    {
+        pctname: 'Marbling',
+        pctpic: '#',
+        pctdesc:'This is a description for casey'
+    },
+];
 
-var keyPress = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var userKeyPress;
+var keyPress = '';
+var userKeyPress = '';
 var isTired = True;
+
+var pctname = gameWords[0].pctname;
+var pctpic = gameWords[0].pctpic;
+
+
+var userKeyPress = [];
+
+
+var name = 'JJ Harry';
+
+// pre es6 concatenation
+console.log('The best person ever is '+ name);
+
+// es6 template literals
+console.log(`The best person ever is ${name}`);
+
+
+var compIndexItem = Math.floor(Math.random()*4);
+
+var randomGameWords = gameWords[compIndexItem].pctname;
